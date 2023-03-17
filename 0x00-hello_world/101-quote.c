21a0bcf78ae697c984cf2e3e37191735d32e84aa
kiyoonewton <kiyoonewton41@gmail.com>
2023-03-16 19:53:36 -0700

    Add 101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - main function
  *
- * Return: Always 0 (Success)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	return (0);
+	system("echo 'and that piece of art is useful\" \- Dora Korpar, 2015-10-19'");
+	return (1);
 }
