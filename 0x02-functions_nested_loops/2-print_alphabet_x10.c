318e95fb96482fc55bbafaba0c539f278f79003b
kiyoonewton <kiyoonewton41@gmail.com>
2023-03-22 05:40:44 -0700

    Correction for betty in print alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,15 +8,12 @@
 
 void print_alphabet_x10(void)
 {
-    char a;
-    int i = 0;
-    while (i < 10)
-    {
-        for (a = 'a'; a <= 'z'; a++)
-        {
-            _putchar(a);
-        }
-        _putchar('\n');
-        i++;
-    }
+	int line, ch;
+
+	for (line = 0; line <= 9; ++line)
+	{
+		for (ch = 'a'; ch <= 'z'; ++ch)
+			_putchar(ch);
+		_putchar('\n');
+	}
 }
