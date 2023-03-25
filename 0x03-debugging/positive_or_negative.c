489eb2d2d8dd91bf4f2bd374701bf2d73a742f8b
kiyoonewton <kiyoonewton41@gmail.com>
2023-03-24 18:49:20 -0700

    let's debug

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print if digit is positive or negative
+ *
+ * Return: Always 0 (Success)
+*/
+
+void positive_or_negative(int i)
+{
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else
+	{
+		printf("%d is negative\n", i);
+	}
+}
