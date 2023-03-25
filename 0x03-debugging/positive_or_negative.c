755118710e562a3b5e523b44da5379cb7272e829
Isaac <kiyoonewton41@gmail.com>
2023-03-25 02:21:03 +0100

    Update positive_or_negative.c
    Update positive or negative

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -3,19 +3,14 @@
 /**
  * main - Entry point
  *
- * Description: print value of n satus; zero, positive or negative
+ * Description: print if digit is positive or negative
  *
  * Return: Always 0 (Success)
 */
 
 void positive_or_negative(int i)
 {
-	/* my if statement */
-	if (i == 0)
-	{
-		printf("%d is zero\n", i);
-	}
-	else if (i > 0)
+	if (i > 0)
 	{
 		printf("%d is positive\n", i);
 	}
