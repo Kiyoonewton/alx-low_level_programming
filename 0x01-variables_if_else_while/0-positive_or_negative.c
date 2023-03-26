a7abc42c9c18de4c247d3015eaee80602bca3cab
kiyoonewton <kiyoonewton41@gmail.com>
2023-03-26 05:16:04 -0700

    Checkif digit is positive or negative or zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,10 +2,11 @@
 #include <time.h>
 #include <stdio.h>
 
+/* betty style doc for function main goes there */
 /**
- *main - main function
+ * main - Main Entry
  *
- * Return: Always (0) success
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -13,14 +14,14 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
-	if (n > 0)
-
-
-		printf("%d is positive\n", n);
-	else if (n < 0)
-		printf("%d is negative\n", n);
-	else
+	/* your code goes there */
+	if (n == 0)
 		printf("%d is zero\n", n);
+	else if (n > 0)
+		printf("%d is positive\n", n);
+	else
+
+		printf("%d is negative\n", n);
+
 	return (0);
 }
