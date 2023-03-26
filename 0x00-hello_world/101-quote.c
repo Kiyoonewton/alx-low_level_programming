c286c3dd49a5c95703d0ab7d9043414d0fe7755f
kiyoonewton <kiyoonewton41@gmail.com>
2023-03-26 04:21:22 -0700

    C language print without put or printf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,10 @@
 
 /**
  * main - Main Entry
- *
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-	system("echo 'and that piece of art is useful\" - Dora Korpar, 2015-10-19' \n");
+	system("echo 'and that piece of art is useful\" - Dora Korpar, 2015-10-19'");
 	return (1);
 }
