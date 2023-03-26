791a624701a1f5abfdd61de2e8fbfb51bdfb0dbc
kiyoonewton <kiyoonewton41@gmail.com>
2023-03-26 06:11:03 -0700

    Print all alphabet

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,23 +3,24 @@
 #include <stdio.h>
 
 /**
- *main - main function
+ * main - Main print if positive or negative of the value inputted
  *
- * Return: Always (0) success
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	int n, l;
+	int n, last;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	l = n % 10;
-
-	if (l > 5)
-		printf("Last digit of %d is %d and is greater than 5\n", n, l);
-	else if (l == 0)
-		printf("Last digit of %d is %d and is 0\n", n, l);
-	else if (l != 0 && l < 6)
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
+	last = n % 10;
+	/* your code goes there */
+	if (last > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, last);
+	else if (last == 0)
+		printf("Last digit of %d is %d and is 0\n", n, last);
+	else
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
 	return (0);
 }
