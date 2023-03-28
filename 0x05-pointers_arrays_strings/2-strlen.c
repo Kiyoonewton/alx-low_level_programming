861f6c6c1ce5a827c642ff4eb5f9975ae7ae37c3
kiyoonewton <kiyoonewton41@gmail.com>
2023-03-27 17:01:45 -0700

    Get the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen - determine the length of a string
+ *
+ * @s: char pointer
+ *
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int a;
+
+	a = strlen(s) - 1;
+	return (a);
+}
