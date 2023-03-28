e649e7d977787c44c528111adcbb77b903c2a7ff
kiyoonewton <kiyoonewton41@gmail.com>
2023-03-27 17:44:11 -0700

    Reverse a string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -13,8 +13,9 @@ void print_rev(char *s)
 	int i;
 
 	for (i = 0; s[i] != '\0'; ++i)
+		;
 
-		for (--i; i >= 0; --i)
-			_putchar(s[i]);
-		_putchar('\n');
+	for (--i; i >= 0; --i)
+		_putchar(s[i]);
+	_putchar('\n');
 }
