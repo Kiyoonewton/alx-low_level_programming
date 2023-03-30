621e4427c4dba057e7c14d3fbb676cc11280529a
kiyoonewton <kiyoonewton41@gmail.com>
2023-03-30 01:22:26 -0700

    Using strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,6 +4,10 @@
 /**
  * _strcat - concatenates two strings
  *
+ * @dest: a pointer the character we eant to concat
+ *
+ * @src: a pointer the character to concat with
+ *
  * Return: nil
  */
 char *_strcat(char *dest, char *src)
