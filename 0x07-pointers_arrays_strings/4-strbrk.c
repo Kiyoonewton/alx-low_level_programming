cea3dff15561641178e05b748888bef9fb3df15c
kiyoonewton <kiyoonewton41@gmail.com>
2023-04-03 20:28:49 -0700

    Strbrk condition is added

---
+++ 0x07-pointers_arrays_strings/4-strbrk.c
@@ -0,0 +1,28 @@
+/**
+ * _strpbrk - a function that searches a
+ *            string for any of a set of characters.
+ *
+ * @s: pointer to input string
+ * @accept: the condition for acceptance
+ *
+ * Return: return @s or NULL
+*/
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+				return (s + i);
+			j++;
+		}
+		i++;
+	}
+	return ('\0');
+}
