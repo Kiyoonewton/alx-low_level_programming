6ccb8dc335b1e72ca8485016e31346787f5d8fcf
kiyoonewton <kiyoonewton41@gmail.com>
2023-04-10 12:44:54 -0700

    Add whatsmyname and args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Print out the number of argument passed
+ *
+ * @argc: the number of argument passed
+ *
+ * @argv: the arrays
+ *
+ * Return: return (0) success
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n:", argc - 1);
+	return (0);
+}
