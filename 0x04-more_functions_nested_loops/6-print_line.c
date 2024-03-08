ff54c4639bdcc77cdfc48d22e2928fc96a7a3699
Kiyoonewton <kiyoonewton41@gmail.com>
2024-03-08 14:06:47 +0100

    Improve on the print line function

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,22 +1,20 @@
-#include"main.h"
+#include "main.h"
 
 /**
  * print_line - print a straight line
  *
  * @n: is the number of times the _ character
  *     should be printed
-*/
+ */
 
 void print_line(int n)
 {
 	int lnChr;
 
-	if (n <= 0)
-		_putchar('\n');
-	else
+	if (n >= 0)
 	{
 		for (lnChr = 1; lnChr <= n; ++lnChr)
 			_putchar('_');
-		_putchar('\n');
 	}
+	_putchar('\n');
 }
